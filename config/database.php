ac41cbed646944b6784370a571e03b4b4d00c5bf
obelixx1984 <karbowiakk@wp.pl>
2026-05-13 20:33:07 +0200

    demo2

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST',    'localhost');
-define('DB_NAME',    'modul_serwis');
+define('DB_NAME',    'cmms2');
 define('DB_USER',    'root');
 define('DB_PASS',    '');
 define('DB_CHARSET', 'utf8mb4');
